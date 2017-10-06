5ae771863aea08a578e5b3af6a7e9b48f6369f60
KuNman <knejman56@gmail.com>
2017-10-06 21:36:52 +0200

    Comments fixed

--- Index.php
+++ Index.php
@@ -6,14 +6,15 @@ require_once 'Disease.php';
 $patient = new Patient('Adam Nowak', 112, 111);
 // connect to db
 $query = new Db();
-// execute query parsed by Patient method
+// save new patient
 $result = $query->query($patient->savePatientToDB());
 // set or update height
 $result = $query->query($patient->setHeight(2, 150));
 // set or update weight
 $result = $query->query($patient->setWeight(1,50));
+// add new disease with patients id
 $disease = new Disease('cancer', 1, '2017-01-10', 'chemotherapy');
-// add patient's disease in diseases table
+// add patients disease in diseases table
 $result = $query->query($disease->saveDiseaseToDB());
 //search patients info
 $patientsIdArray = [1,3,60];
